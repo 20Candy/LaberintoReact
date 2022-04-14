1083a62643475b0fc2269f135734df611d18f7db
Carol Arevalo <carol.arevalo@ext.firstlight.ai>
2022-04-14 17:16:09 -0600

    gif better jejej

--- src/spinner.jsx
+++ src/spinner.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import GifLoader from 'react-gif-loader';
 
 function Spinner(){
+
     return (
-        <h1> Loading the adventure...
-        <GifLoader
-            loading={true}
-            imageSrc="./img/ori.gif"
-            overlayBackground="rgba(0,0,0,0.5)"
-        />
-        </h1>
+        <div>
+            <h1 className='loader'> Loading the adventure...</h1>
+            <img src="./img/ori.gif"/>
+        </div>
+
 
     );
 
